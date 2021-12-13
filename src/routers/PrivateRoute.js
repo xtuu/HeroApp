6364fd26affe8df888bd3346b681206df4925a38
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2021-12-13 16:30:39 -0400

    [NEW] Nuevo rutas privadas

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { AuthContext } from "../auth/authContext"
+
+
+
+export const PrivateRoute = ({ children }) => {
+
+    const { user } = useContext(AuthContext)
+
+    return user.logged ? children : <Navigate to='/login' /> 
+}
